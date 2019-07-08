92671a2c3b2a663df884696aa5af2159dbd796b2
YoungBeenKim <brokentime906@gmail.com>
2019-07-09 01:04:00 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import React ,{Component} from 'react';
 import './App.css';
 import Customer from './components/Customer'
 
+const customers =[
+{
+  'id' :1,
+  'imgae' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : 'student'
+}  ,
+{
+  'id' :2,
+  'imgae' : 'https://placeimg.com/64/64/2',
+  'name' : '김영빈',
+  'birthday' : '951222',
+  'gender' : '남자',
+  'job' : 'student'
+}
+]
 class App extends Component{
   render(){
     return (  
-      <Customer/>
-  
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.imgae}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.gender}
+              />
+            );
+          })
+        }
+
+      </div>
     );
   } 
 }
